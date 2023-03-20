3f0be2d31a3b101df11dcaa2d38a8b87bd8e6656
Spencer Elliott <s.elliott123@gmail.com>
2023-03-20 15:44:36 -0500

    Moved usleep to its own module

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,20 @@
 #![no_std]
 
 mod serial;
+mod time;
 
 use crate::serial::Serial;
-
-extern "C" {
-    pub fn usleep(ns: i32);
-}
+use crate::time::Time;
 
 #[no_mangle]
 pub extern "C" fn main() {
     Serial::init(56700);
 
-    unsafe {
-        usleep(1500000);
-    }
+    Time::usleep(1500000);
 
     Serial::report("Hello from Rust report()\n\r");
     Serial::report("Another report()\n\r");
-    Serial::report("Another test for funsies\n\r");
+    Serial::report("Another test fun for funsies\n\r");
 
-    unsafe {
-        usleep(2000);
-    }
+    Time::usleep(2000);
 }
--- src/serial.rs
+++ src/serial.rs
@@ -10,24 +10,29 @@ extern "C" {
 pub struct Serial { }
 
 impl Serial {
+    /// Initialize the serial interface with the desired baud rate.
     pub fn init(baud: i32) {
         unsafe {
             serial_init(baud);
         }
     }
 
+    /// Sends a single character to the serial port. A call to `Serial::flush()` may be
+    /// required to actually write the data.
     pub fn putc(c: i32) {
         unsafe {
             serial_putc(c);
         }
     }
 
+    /// Flushes any outstanding data out to the serial port.
     pub fn flush() {
         unsafe {
             serial_flush();
         }
     }
 
+    /// Writes a string out to the serial port. A call to `Serial::flush()` is not required.
     pub fn report(s: &str) {
         unsafe {
             report(s);
